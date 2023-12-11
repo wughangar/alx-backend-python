569412ff1452577ae503d3e0b6efd07da013c61e
wughangar <wughangar.rose@gmail.com>
2023-12-11 12:23:04 +0000

    added a return value

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -13,3 +13,5 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     tasks = [wait_random(max_delay) for _ in range(n)]
     results = await asyncio.gather(*tasks)
+
+    return sorted(results)
