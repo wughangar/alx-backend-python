62b3c3b5fec0f67781b52390f8ae3b33ce41acd0
wughangar <wughangar.rose@gmail.com>
2023-12-12 09:02:22 +0000

    imported generator

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -3,10 +3,11 @@
 1. Async Comprehensions
 """
 import asyncio
+from typing import Generator
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> float:
+async def async_comprehension() -> Generator[float, None, None]:
     """
     function that returns 10 random numbers from async_generator
     """
