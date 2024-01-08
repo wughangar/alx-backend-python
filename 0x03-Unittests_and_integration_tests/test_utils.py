7a27da897526d997d3866a65f4eba14f042f8906
wughangar <wughangar.rose@gmail.com>
2024-01-08 07:46:17 +0000

    exception error tested

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -13,14 +13,30 @@ class TestAccessNestedMap(unittest.TestCase):
     """
     class that tests the acess to nested map
     """
-    @parameterized.expand([
-        ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2)
-        ])
+
+    @parameterized.expand(
+        [
+            ({"a": 1}, ("a",), 1),
+            ({"a": {"b": 2}}, ("a",), {"b": 2}),
+            ({"a": {"b": 2}}, ("a", "b"), 2),
+        ]
+    )
     def test_access_nested_map(self, nested_map: Mapping, path: Sequence,
                                expected_result: Union[Dict, int]) -> None:
         """
         method to test that the method returns what it is supposed to.
         """
         self.assertEqual(access_nested_map(nested_map, path), expected_result)
+
+    @parameterized.expand([
+        ({}, ("a"), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError)
+        ])
+    def test_access_nested_map_exception(
+        self, nested_map: Mapping, path: Sequence, exception: Exception
+    ) -> None:
+        """
+        method that testsd access to nested map exception
+        """
+        with self.assertRaises(exception):
+            access_nested_map(nested_map, path)
