47b2a1d6bf77c3f070b1d718f447ba142f362b5b
wughangar <wughangar.rose@gmail.com>
2024-01-08 07:13:48 +0000

    readme file

---
+++ 0x03-Unittests_and_integration_tests/README.md
@@ -0,0 +1,13 @@
+0x03. Unittests and Integration Tests
+Learning Objectives
+At the end of this project, you are expected to be able to explain to anyone, without the help of Google:
+
+The difference between unit and integration tests.
+Common testing patterns such as mocking, parametrizations and fixtures
+
+All your files will be interpreted/compiled on Ubuntu 18.04 LTS using python3 (version 3.7)
+All your files should end with a new line
+The first line of all your files should be exactly #!/usr/bin/env python3
+A README.md file, at the root of the folder of the project, is mandatory
+Your code should use the pycodestyle style (version 2.5)
+All your files must be executable
---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+test file
+"""
+
+import unittest
+from parameterized import parameterized, parameterized_class
+from utils import access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """
+    class that tests the acess to nested map
+    """
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), 2),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+        ])
+
+
+    def test_access_nested_map(self, nested_map, path, expected_result):
+        """
+        method to test that the method returns what it is supposed to.
+        """
+        self.assertEqual(access_nested_map(nested_map, path), expected_result)
