ccc2b79eae33eafe7f97e5afbf9c256e34bb4fbb
wughangar <wughangar.rose@gmail.com>
2024-01-08 08:54:38 +0000

    mock https calls

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -7,6 +7,9 @@ import unittest
 from parameterized import parameterized, parameterized_class
 from utils import access_nested_map
 from typing import Dict, Mapping, Sequence, Tuple, Union
+from unittest.mock import patch
+from unittest.mock import Mock
+from utils import get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -40,3 +43,27 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         with self.assertRaises(exception):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """
+    class to test test_get_json method
+    """
+    @patch('utils.requests.get')
+    @patch('utils.get_json')
+    def test_get_json(self, mock_get_json, mock_requests_get):
+        """
+        mock function to test getting urls
+        """
+        test_cases = [
+                ("http://example.com", {"payload": True}),
+                ("http://holberton.io", {"payload": False})
+                ]
+        for test_url, test_payload in test_cases:
+            mock_response = Mock()
+            mock_response.json.return_value = test_payload
+            mock_requests_get.return_value = mock_response
+
+            result = get_json(test_url)
+            mock_requests_get.assert_called_with(test_url)
+            self.assertEqual(result, test_payload)
